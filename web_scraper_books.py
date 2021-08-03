1adc0688582e5b7ef14d7bcb5cf36d665ffcf86d
Olivier RIO <opm.rio@gmail.com>
2021-08-03 11:19:30 +0200

    Séparation des fonctions d'extraction et de transformation

--- web_scraper_books.py
+++ web_scraper_books.py
@@ -17,28 +17,27 @@ from bs4 import BeautifulSoup
 from requests import get
 
 
-def get_url_categories(page, url):
+def get_url_categories(page):
     # get the URL's of all categories of books and store them in a list
     soup_categories = BeautifulSoup(page.content, 'html.parser')
-    url_categories = [urljoin(url, i["href"]) for i in soup_categories.find("ul",
-                                                                            class_="nav").find_next("ul").find_all("a")]
-    return url_categories
+    url_raw_categories = [i["href"] for i in soup_categories.find("ul", class_="nav").find_next("ul").find_all("a")]
+    return url_raw_categories
 
 
 def get_url_books(page, url, books):
     # get the URL's of all books in the category and store them in a list
     soup_books = BeautifulSoup(page.content, 'html.parser')
-    books.extend([urljoin(url, i.find("a")["href"]) for i in soup_books.find_all("article")])
+    books.extend([i.find("a")["href"] for i in soup_books.find_all("article")])
     if soup_books.find("li", class_="next"):
-        next_url = urljoin(url, soup_books.find("li", class_="next").find_next("a")["href"])
+        next_url_raw = soup_books.find("li", class_="next").find_next("a")["href"]
+        next_url = transfo_url(url, next_url_raw)
         next_page = get(next_url)
         get_url_books(next_page, next_url, books=books)
     else:
         pass
     return books
 
-
-def get_data_book(page, url, url_category):
+def get_data_book(page, url):
     # get all the required data on a book and store them in a dictionary
     soup_book = BeautifulSoup(page.content, 'html.parser')
     book_dict = dict()
@@ -54,23 +53,35 @@ def get_data_book(page, url, url_category):
         book_dict["product_description"] = ""
     book_dict["category"] = soup_book.find("a", text="Books").find_next("a").text
 
-    review_rating_text = soup_book.find("p", {'class': lambda x: "star-rating" in x.split()})["class"].pop()
-    number_dict = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
-    book_dict["review_rating"] = number_dict[review_rating_text]
+    book_dict["review_rating"] = soup_book.find("p", {'class': lambda x: "star-rating" in x.split()})["class"].pop()
     book_dict["image_url"] = urljoin(url, soup_book.find("img")["src"])
-    name_image = book_dict["title"]
-    name_image = re.sub('[<>/:"|?*,\\\\]', "_", name_image)
-    file_image = get_file_image(name_image, soup_book, url, url_category)
-    book_dict["file_image"] = '"' + os.path.join(os.getcwd(), file_image[0]) + '"'
+    book_dict["file_image"] = soup_book.find("img")["src"]
     return book_dict
 
 
-def get_file_image(name, soup, url, url_category):
+def get_file_image(name, url_image, url, url_category):
     # get the image_file of the book on the web
     # save this in a local directory with an evocative name
-    file_image = urllib.request.urlretrieve(urljoin(url, soup.find("img")["src"]),
+    file_image = urllib.request.urlretrieve(urljoin(url, url_image),
                                             f'./Web_scraper/{url_category.split("/")[-2]}/{name}.jpg')
     return file_image
+
+
+def transfo_url(url, url_raws):
+    if type(url_raws) == list:
+        return [urljoin(url, url_raw) for url_raw in url_raws]
+    elif type(url_raws) == str:
+        return urljoin(url, url_raws)
+
+
+def transfo_data_book(raw_data_book, url, url_category):
+        raw_data_book["image_url"] = transfo_url(url, raw_data_book["image_url"])
+        name_image = re.sub('[<>/:"|?*,\\\\]', "_", raw_data_book["title"])
+        file_image = get_file_image(name_image, raw_data_book["file_image"], url, url_category)
+        raw_data_book["file_image"] = '"' + os.path.join(os.getcwd(), file_image[0]) + '"'
+        number_dict = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
+        raw_data_book["review_rating"] = number_dict[raw_data_book["review_rating"]]
+        return raw_data_book
 
 
 def create_directory(name):
@@ -107,15 +118,20 @@ def main():
     # main function of the program
     URL_HOME = "http://books.toscrape.com"
     page_categories = get(URL_HOME)
-    url_categories = get_url_categories(page_categories, URL_HOME)
+    url_raw_categories = get_url_categories(page_categories)
+    url_categories = transfo_url(URL_HOME, url_raw_categories)
     for url_category in url_categories:
-        category_dict = {}
+        category_raw_dict = dict()
+        category_dict = dict()
         page_books = get(url_category)
         create_directory(url_category)
-        url_books = get_url_books(page_books, url_category, books=[])
+        url_raw_books = get_url_books(page_books, url_category, books=[])
+        url_books = transfo_url(url_category, url_raw_books)
         for url_book in url_books:
             page_book = get(url_book)
-            category_dict[url_books.index(url_book)] = get_data_book(page_book, url_book, url_category)
+            category_raw_dict[url_books.index(url_book)] = get_data_book(page_book, url_book)
+            category_dict[url_books.index(url_book)] = transfo_data_book(category_raw_dict[url_books.index(url_book)],
+                                                                         url_book, url_category)
             print(url_book)
         create_csv(category_dict, url_category)
 
